3cf854c033c7e79b039b0a1d5e2ccf08661e080e
TravisDC5 <61341968+TravisDC5@users.noreply.github.com>
2020-05-25 00:39:07 -0700

    app.py nearign completion

--- app.py
+++ app.py
@@ -50,8 +50,8 @@ def home():
             f"/api/v1.0/stations <br>"
             f"/api/v1.0/precipitation <br>"
             f"/api/v1.0/temperature <br>"
-            f"/api/v1.0/<start> <br>"
-            f"/api/v1.0/<start>/<end>"
+            f"/api/v1.0/2016-08-23 <br>"
+            f"/api/v1.0/2016-08-23/2017-08-23"
     )
 
 @app.route("/api/v1.0/stations")
@@ -100,7 +100,7 @@ def temperature():
 
     return jsonify(tempList)
 
-@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<yearAgo>")
 def start(yearAgo):
     session = Session(engine)
     select = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
@@ -122,7 +122,25 @@ def start(yearAgo):
         dates.append(date_dict)
     return jsonify(dates)
     
+@app.route('/api/v1.0/<yearAgo>/<maxDate>')
+def startEnd(yearAgo, maxDate):
+    sel = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
 
+    results =  (session.query(*sel)
+                       .filter(func.strftime("%Y-%m-%d", Measurement.date) >= yearAgo)
+                       .filter(func.strftime("%Y-%m-%d", Measurement.date) <= maxDate)
+                       .group_by(Measurement.date)
+                       .all())
+
+    dates = []                       
+    for result in results:
+        date_dict = {}
+        date_dict["Date"] = result[0]
+        date_dict["Low Temp"] = result[1]
+        date_dict["Avg Temp"] = result[2]
+        date_dict["High Temp"] = result[3]
+        dates.append(date_dict)
+    return jsonify(dates)
 
 if __name__ == '__main__':
     app.run(debug=True)
